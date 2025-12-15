b9d79c18c72c8460bacfd29c1d9ada161948747c
Ikechukwu  Osuya <100960483+Hykze1@users.noreply.github.com>
2025-12-15 11:26:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.filterwarnings("ignore", category=pd.errors.SettingWithCopyWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-image_url = "download (27).png"
+image_url = "pics.png"
 st.image(image_url, caption="Forest ecosystem", use_container_width=True)
 
 st.header(' Rwanda Wetland/ Forest Analaysis')
@@ -6693,6 +6693,7 @@ with tabs[1]:
     # Display HTML map inside Streamlit
     html_file = open("Rwanda_Forests_Ecosystem_Services_Map.html", 'r', encoding='utf-8')
     components.html(html_file.read(), height=600)
+
 
 
 
